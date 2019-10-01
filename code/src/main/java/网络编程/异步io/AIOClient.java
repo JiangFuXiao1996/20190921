c2259707c045f5175faebf055897924a43bda040
JiangLai <314544729@qq.com>
2019-10-01 19:00:58 +0800

    网络编程

--- code/src/main/java/网络编程/异步io/AIOClient.java
+++ code/src/main/java/网络编程/异步io/AIOClient.java
@@ -1,10 +1,45 @@
 package 网络编程.异步io;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+import java.nio.charset.Charset;
+import java.util.concurrent.Future;
+
 /**
  * Created by 拂晓 on 2019/10/1:17:58
  */
 public class AIOClient {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final AsynchronousSocketChannel client = AsynchronousSocketChannel.open();
+        client.connect(new InetSocketAddress("localhost", 8989), null, new CompletionHandler<Void, Object>() {
+
+            @Override
+            public void completed(Void result, Object attachment) {
+                try {
+                    System.out.println("连接服务器成功");
+                    ByteBuffer buffer = ByteBuffer.allocate(20);
+                    Future<Integer> f = client.read(buffer);
+                    f.get();
+                    buffer.flip();
+                    String str = Charset.defaultCharset().decode(buffer).toString();
+                    System.out.println(str);
+                    client.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void failed(Throwable exc, Object attachment) {
+
+            }
+        });
+
+        Thread.sleep(2000);
+
 
     }
 }
--- code/src/main/java/网络编程/异步io/AIOServer.java
+++ code/src/main/java/网络编程/异步io/AIOServer.java
@@ -1,12 +1,51 @@
 package 网络编程.异步io;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
 /**
  * Created by 拂晓 on 2019/10/1:17:57
  */
 public class AIOServer {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
+        final AsynchronousServerSocketChannel server = AsynchronousServerSocketChannel.open();
 
+        server.bind(new InetSocketAddress(8989));
+
+        server.accept(null, new CompletionHandler<AsynchronousSocketChannel, Object>() {
+
+            @Override
+            public void completed(AsynchronousSocketChannel result, Object attachment) {
+                try {
+                    System.out.println("连接成功");
+                    ByteBuffer buffer = ByteBuffer.wrap("Hello".getBytes());
+                    Future<Integer> f = result.write(buffer);
+                    f.get();
+                    result.close();
+                    server.accept(null, this);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void failed(Throwable exc, Object attachment) {
+            }
+        });
+
+        System.out.println("main go on");
+
+        while (true) {
+            Thread.sleep(1000);
+        }
+        
     }
 
 }
